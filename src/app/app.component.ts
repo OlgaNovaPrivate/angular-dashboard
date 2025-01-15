a85e5594e7919824a2aa52175ef9815415618bf0
OlgaNovaPrivate <149007018+OlgaNovaPrivate@users.noreply.github.com>
2025-01-15 20:28:39 +0200

    Add animation route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,127 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
+import {
+  animate,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
   imports: [RouterOutlet, TabsComponent],
+  animations: [
+    trigger('routeAnim', [
+      transition(':increment', [
+        style({ position: 'relative', overflow: 'hidden' }),
+
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              opacity: 0,
+            }),
+          ],
+          { optional: true }
+        ),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '150ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'translateX(-20px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({
+                opacity: 0,
+                transform: 'translateX(20px)',
+              }),
+              animate(
+                '150ms ease-in',
+                style({ opacity: 1, transform: 'translateX(0)' })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+
+      transition(':decrement', [
+        style({ position: 'relative', overflow: 'hidden' }),
+
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              opacity: 0,
+            }),
+          ],
+          { optional: true }
+        ),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '150ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'translateX(20px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({
+                opacity: 0,
+                transform: 'translateX(-20px)',
+              }),
+              animate(
+                '150ms ease-in',
+                style({ opacity: 1, transform: 'translateX(0)' })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'angular-dashboard';
+  title?: string;
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet.isActivated &&
+      outlet.activatedRouteData['tabIndex'] !== undefined
+      ? outlet.activatedRouteData['tabIndex']
+      : 0;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { TodosComponent } from './todos/todos.component';
 import { NotesComponent } from './notes/notes.component';
 
 export const routes: Routes = [
-  { path: 'bookmarks', component: BookmarksComponent },
-  { path: 'todos', component: TodosComponent },
-  { path: 'notes', component: NotesComponent },
+  { path: 'bookmarks', component: BookmarksComponent, data: { tabIndex: 0 } },
+  { path: 'todos', component: TodosComponent, data: { tabIndex: 1 } },
+  { path: 'notes', component: NotesComponent, data: { tabIndex: 2 } },
 ];
