6d3f46df9e8373fa72709b9fa0fb72fea9115118
OlgaNovaPrivate <149007018+OlgaNovaPrivate@users.noreply.github.com>
2025-03-15 18:34:48 +0200

    Create Add bookmark view

---
+++ src/app/add-bookmark/add-bookmark.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { BookmarkService } from '../shared/bookmark/bookmark-service/bookmark.service';
+import { Bookmark } from '../shared/bookmark/bookmark-model/bookmark.model';
+
+@Component({
+  selector: 'app-add-bookmark',
+  standalone: true,
+  imports: [FormsModule, NgIf, RouterLink],
+  templateUrl: './add-bookmark.component.html',
+  styleUrl: './add-bookmark.component.scss',
+})
+export class AddBookmarkComponent {
+  constructor(
+    private bookmarkService: BookmarkService,
+    private router: Router
+  ) {}
+
+  onFormSubmit(form: NgForm) {
+    const { name, url } = form.value;
+    const bookmark = new Bookmark(name, url);
+    this.bookmarkService.addBookmark(bookmark);
+
+    this.router
+      .navigateByUrl('/bookmarks')
+      .then(() => {
+        console.log('Navigation successful');
+      })
+      .catch(err => {
+        console.error('Navigation error', err);
+      });
+
+    return;
+  }
+}
