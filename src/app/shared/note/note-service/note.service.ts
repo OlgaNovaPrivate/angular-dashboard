31cd9fb9a123ded4fc54f2532187e738c8530f80
OlgaNovaPrivate <149007018+OlgaNovaPrivate@users.noreply.github.com>
2025-03-25 20:22:17 +0200

    Saving Notes State to Local Storage

--- src/app/shared/note/note-service/note.service.ts
+++ src/app/shared/note/note-service/note.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Note } from '../note-model/note.model';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NoteService {
-  notes: Note[] = [
-    new Note('Test Title', 'Test Content'),
-    new Note('Test Title2', 'Test Content2'),
-  ];
+export class NoteService implements OnDestroy {
+  notes: Note[] = [];
 
-  constructor() {}
+  storageListenSub: Subscription;
+
+  constructor() {
+    this.loadState();
+
+    this.storageListenSub = fromEvent<StorageEvent>(
+      window,
+      'storage'
+    ).subscribe(event => {
+      if (event.key === 'notes') this.loadState();
+      console.log('Storage event fired!');
+      console.log(event);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.storageListenSub) this.storageListenSub.unsubscribe();
+  }
 
   getNotes() {
     console.log('getNotes called in service');
@@ -23,6 +38,8 @@ export class NoteService {
 
   addNote(note: Note) {
     this.notes.push(note);
+
+    this.saveState();
   }
 
   updateNote(id: string, updatedFields: Partial<Note>) {
@@ -30,6 +47,8 @@ export class NoteService {
     note
       ? Object.assign(note, updatedFields)
       : console.error(`Note with id ${id} not found`);
+
+    this.saveState();
   }
 
   deleteNote(id: string) {
@@ -37,5 +56,29 @@ export class NoteService {
     if (noteIndex == -1) return;
 
     this.notes.splice(noteIndex, 1);
+
+    this.saveState();
+  }
+
+  saveState() {
+    localStorage.setItem('notes', JSON.stringify(this.notes));
+  }
+
+  loadState() {
+    try {
+      const notesInStorage = localStorage.getItem('notes');
+      const parsedNotes: Note[] = notesInStorage
+        ? JSON.parse(notesInStorage)
+        : [];
+
+      this.notes.length = 0;
+      this.notes.push(...parsedNotes);
+    } catch (error) {
+      console.error(
+        'There was an error retrieving notes from local storage!',
+        error
+      );
+      this.notes = [];
+    }
   }
 }
