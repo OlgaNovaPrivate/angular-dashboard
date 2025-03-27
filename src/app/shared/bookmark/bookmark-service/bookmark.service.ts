4b99ec7eaf924dadecdaa283b102bfe671007532
OlgaNovaPrivate <149007018+OlgaNovaPrivate@users.noreply.github.com>
2025-03-27 21:20:48 +0200

    Saving Bookmarks to Local Storage

--- src/app/shared/bookmark/bookmark-service/bookmark.service.ts
+++ src/app/shared/bookmark/bookmark-service/bookmark.service.ts
@@ -1,18 +1,27 @@
 import { Bookmark } from '../bookmark-model/bookmark.model';
 import { Injectable } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookmarkService {
-  bookmarks: Bookmark[] = [
-    new Bookmark('Google', 'https://google.com'),
-    new Bookmark('Youtube', 'https://youtube.com'),
-    new Bookmark('Twitch', 'https://twitch.com'),
-    new Bookmark('Tiktok', 'https://tiktok.com'),
-  ];
+  bookmarks: Bookmark[] = [];
 
-  constructor() {}
+  storageListenSub: Subscription;
+
+  constructor() {
+    this.loadState();
+
+    this.storageListenSub = fromEvent<StorageEvent>(
+      window,
+      'storage'
+    ).subscribe(event => {
+      if (event.key === 'bookmarks') this.loadState();
+      console.log('Storage event fired!');
+      console.log(event);
+    });
+  }
 
   getBookmarks() {
     return this.bookmarks;
@@ -24,6 +33,8 @@ export class BookmarkService {
 
   addBookmark(bookmark: Bookmark) {
     this.bookmarks.push(bookmark);
+
+    this.saveState();
   }
 
   updateBookmark(id: string, updateFields: Partial<Bookmark>) {
@@ -31,11 +42,40 @@ export class BookmarkService {
     bookmark
       ? Object.assign(bookmark, updateFields)
       : console.error(`Note with id ${id} not found`);
+
+    this.saveState();
   }
 
   deleteBookmark(id: string) {
     const bookmarkIndex = this.bookmarks.findIndex(b => b.id !== id);
     if (bookmarkIndex == -1) return;
     this.bookmarks.splice(bookmarkIndex, 1);
+
+    this.saveState();
+  }
+
+  saveState() {
+    localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
+  }
+
+  loadState() {
+    try {
+      const bookmarksInStorage = localStorage.getItem('bookmarks');
+      if (!bookmarksInStorage) {
+        this.bookmarks = [];
+        return;
+      }
+      const parsedBookmarks: Bookmark[] = JSON.parse(
+        bookmarksInStorage,
+        (key, value) => (key === 'url' ? new URL(value) : value)
+      );
+      this.bookmarks.push(...parsedBookmarks);
+    } catch (error) {
+      console.error(
+        'There was an error retrieving bookmarks from local storage!',
+        error
+      );
+      this.bookmarks = [];
+    }
   }
 }
