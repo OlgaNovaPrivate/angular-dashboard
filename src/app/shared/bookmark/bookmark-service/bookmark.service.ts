967a25a699e8d59a8e637c35f617869dafc114d1
OlgaNovaPrivate <149007018+OlgaNovaPrivate@users.noreply.github.com>
2025-03-31 21:12:57 +0300

    Debugged Saving Bookmarks to Local Storage

--- src/app/shared/bookmark/bookmark-service/bookmark.service.ts
+++ src/app/shared/bookmark/bookmark-service/bookmark.service.ts
@@ -1,11 +1,11 @@
 import { Bookmark } from '../bookmark-model/bookmark.model';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { fromEvent, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class BookmarkService {
+export class BookmarkService implements OnDestroy {
   bookmarks: Bookmark[] = [];
 
   storageListenSub: Subscription;
@@ -21,6 +21,10 @@ export class BookmarkService {
       console.log('Storage event fired!');
       console.log(event);
     });
+  }
+
+  ngOnDestroy() {
+    this.storageListenSub.unsubscribe();
   }
 
   getBookmarks() {
@@ -39,16 +43,18 @@ export class BookmarkService {
 
   updateBookmark(id: string, updateFields: Partial<Bookmark>) {
     const bookmark = this.getBookmark(id);
-    bookmark
-      ? Object.assign(bookmark, updateFields)
-      : console.error(`Note with id ${id} not found`);
+    if (!bookmark) {
+      console.error(`Bookmark with id ${id} not found`);
+      return;
+    }
+    Object.assign(bookmark, updateFields);
 
     this.saveState();
   }
 
   deleteBookmark(id: string) {
-    const bookmarkIndex = this.bookmarks.findIndex(b => b.id !== id);
-    if (bookmarkIndex == -1) return;
+    const bookmarkIndex = this.bookmarks.findIndex(b => b.id === id);
+    if (bookmarkIndex === -1) return;
     this.bookmarks.splice(bookmarkIndex, 1);
 
     this.saveState();
@@ -61,14 +67,14 @@ export class BookmarkService {
   loadState() {
     try {
       const bookmarksInStorage = localStorage.getItem('bookmarks');
-      if (!bookmarksInStorage) {
-        this.bookmarks = [];
-        return;
-      }
-      const parsedBookmarks: Bookmark[] = JSON.parse(
-        bookmarksInStorage,
-        (key, value) => (key === 'url' ? new URL(value) : value)
-      );
+      const parsedBookmarks: Bookmark[] = bookmarksInStorage
+        ? JSON.parse(bookmarksInStorage, (key, value) => {
+            if (key == 'url') return new URL(value);
+            return value;
+          })
+        : [];
+
+      this.bookmarks.length = 0;
       this.bookmarks.push(...parsedBookmarks);
     } catch (error) {
       console.error(
