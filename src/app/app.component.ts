32ec124ba8fc9e829bcdcecd03533344972b0edd
OlgaNovaPrivate <149007018+OlgaNovaPrivate@users.noreply.github.com>
2025-04-11 21:34:33 +0300

    Added secondary route transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -115,6 +115,54 @@ import { NgIf, NgStyle } from '@angular/common';
           ),
         ]),
       ]),
+
+      transition('* => secondary', [
+        style({ position: 'relative', overflow: 'hidden' }),
+
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              opacity: 0,
+            }),
+          ],
+          { optional: true }
+        ),
+
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '150ms ease-in',
+                style({
+                  opacity: 0,
+                  transform: 'translateX(20px)',
+                })
+              ),
+            ],
+            { optional: true }
+          ),
+          query(
+            ':enter',
+            [
+              style({
+                opacity: 0,
+                transform: 'translateX(-20px)',
+              }),
+              animate(
+                '150ms ease-in',
+                style({ opacity: 1, transform: 'translateX(0)' })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
     ]),
     trigger('bgFadeAnim', [
       transition(':enter', [
@@ -133,10 +181,11 @@ export class AppComponent implements OnInit {
   enableDynamicBg = false;
 
   prepareRoute(outlet: RouterOutlet) {
-    return outlet.isActivated &&
-      outlet.activatedRouteData['tabIndex'] !== undefined
-      ? outlet.activatedRouteData['tabIndex']
-      : 0;
+    if (outlet.isActivated) {
+      const tabIndex = outlet.activatedRouteData['tabIndex'];
+      return tabIndex !== undefined ? tabIndex : 'secondary';
+    }
+    return 'secondary';
   }
 
   ngOnInit() {
@@ -153,10 +202,6 @@ export class AppComponent implements OnInit {
 
   private fallbackBgUrl =
     'https://fastly.picsum.photos/id/450/1920/1080.jpg?hmac=xDeMorMomjhKEbocGtnP89w5iKpSX0h4gMpFaltwJWs';
-
-  setRandomBg(): void {
-    this.bg = this.generateRandomBgUrl();
-  }
 
   private generateRandomBgUrl(): string {
     return `https://picsum.photos/1920/1080?random=${Date.now()}`;
